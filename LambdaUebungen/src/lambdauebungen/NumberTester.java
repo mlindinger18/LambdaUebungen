d84cd347a2a71bed6a4692dd0d528980638f0b71
Marcel Lindinger <mlindinger18@gmail.com>
2021-09-28 14:00:53 +0200

    NumberTester Klassen erstellt

--- LambdaUebungen/src/lambdauebungen/NumberTester.java
+++ LambdaUebungen/src/lambdauebungen/NumberTester.java
@@ -11,4 +11,23 @@ package lambdauebungen;
  */
 public class NumberTester {
 
+    public NumberTester(String filename) {
+        //Todo
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void primeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void palindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+        //Todo
+    }
 }
