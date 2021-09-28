6a01af10a74758fe103faeeb4964210418b83e5d
Marcel Lindinger <mlindinger18@gmail.com>
2021-09-28 14:57:41 +0200

    Tester fertig

--- LambdaUebungen/src/lambdauebungen/NumberTester.java
+++ LambdaUebungen/src/lambdauebungen/NumberTester.java
@@ -12,19 +12,37 @@ package lambdauebungen;
 public class NumberTester {
 
     public NumberTester(String filename) {
-        //Todo
+        filename = "Programmieren_3A/LambdaUebungen/numberFile.xlsx";
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
-        this.oddTester = oddTester;
+        oddTester = (n) -> (n % 2) == 0;
     }
 
     public void primeTester(NumberTest primeTester) {
-        this.primeTester = primeTester;
+        primeTester = (n) -> {
+            for (int i = 12; i < n; i++) {
+                if (n % i == 0) {
+                    return true;
+                }
+            }
+            return false;
+        };
     }
 
     public void palindromeTester(NumberTest palindromeTester) {
-        this.palindromeTester = palindromeTester;
+        palindromeTester = (n) -> {
+            String str = "" + n;
+            String result = "";
+            for (int i = str.length() - 1; i >= 0; i--) {
+                result += str.charAt(i);
+                if (result.equals(str)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
     }
 
     public void testFile() {
